3d8253465df4fbe933c5c33e9c68cd28b9ac93f9
ST04-tkmr <miiko.dsoul@gmail.com>
2023-11-02 19:00:38 +0900

    サーミスタ数調整

--- lib/Thermistor/Thermistor_dfs.hpp
+++ lib/Thermistor/Thermistor_dfs.hpp
@@ -1,14 +1,14 @@
 #ifndef _THERMISTOR_DFS_H_
 #define _THERMISTOR_DFS_H_
 
-const unsigned char ecuNum = 4; // 温度監視ECUの個数(the number of segments)
-const unsigned char adrs[ecuNum] = {
+const unsigned char ecuNum = 1; // 温度監視ECUの個数(the number of segments)
+const unsigned char adrs[4] = {
     0b0000001,
     0b0000010,
     0b0000100,
     0b0001000};
 
-const unsigned char thmNum = 6; // 1つのマイコンで監視するサーミスタの本数（<= 6）
+const unsigned char thmNum = 3; // 1つのマイコンで監視するサーミスタの本数（<= 6）
 
 const float r0 = 10000.0f; // 25℃の時のサーミスタの抵抗値
 const float t0 = 25.0f;    // 基準温度
